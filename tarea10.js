15013f48dd5b49d97de702cd0440540c34b4afa5
jimmyrojasgon <77075586@certus.edu.pe>
2024-06-17 00:05:38 -0500

    codigo promedio

---
+++ tarea10.js
@@ -0,0 +1,27 @@
+class Alummno{
+    codigo;
+    codigo;
+    codigo;
+    codigo;
+    codigo;
+    codigo;
+    
+
+    constructor(codigo){
+        this.codigo = codigo
+    }
+
+    promedio(){
+        return (nota1*15)/100 +
+    }
+    
+    condicion(){
+        if(17<promedio()) return 'aprobado';
+        return 'desaprobado';
+    }
+
+    obsequio(){
+        if(17<promedio()) return 'aprobado'
+        return 'desaprobado';   
+    }
+}
