56c088fc67ec4fc3b0d333a89950fe53f4039a08
jimmyrojasgon <77075586@certus.edu.pe>
2024-06-17 01:24:42 -0500

    avance

--- tarea10.js
+++ tarea10.js
@@ -1,27 +1,31 @@
 class Alummno{
-    codigo;
-    codigo;
-    codigo;
-    codigo;
-    codigo;
-    codigo;
-    
+    var= Codigo;    
+    var= nombre;    
+    var= nota1;
+    var= nota2;
+    var= nota3;
+    var= nota4;    
 
-    constructor(codigo){
-        this.codigo = codigo
+    constructor(Codigo, nombre, nota1, nota2, nota3, nota4){
+        this.codigo = document.getElementById("codigo")
+        this.nombre = document.getElementById("nombre")
+        this.nota1 = document.getElementById("nota1")
+        this.nota2 = document.getElementById("nota2")
+        this.nota3 = document.getElementById("nota3")
+        this.nota4 = document.getElementById("nota4")
     }
 
     promedio(){
-        return (nota1*15)/100 +
+        return (nota1*0.15)+(nota2*0.20)+(nota3*0.25)+(nota4*0.40)/4
     }
     
     condicion(){
-        if(17<promedio()) return 'aprobado';
+        if(12<=promedio()) return 'aprobado';
         return 'desaprobado';
     }
 
     obsequio(){
-        if(17<promedio()) return 'aprobado'
+        if(17<=promedio()) return 'aprobado se gano una mochila'
         return 'desaprobado';   
     }
 }
